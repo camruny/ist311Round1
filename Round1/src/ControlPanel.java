c45349623256349d1b5512266e190c3d2d7e84ea
Shane Eckenrode <sue2@psu.edu>
2014-09-23 18:29:22 -0400

    Replaced Round1JFrame with ControlPanel
    ControlPanel was supposed to be the JFrame based on the Class Diagram.

--- Round1/src/ControlPanel.java
+++ Round1/src/ControlPanel.java
@@ -1,3 +1,20 @@
-public class ControlPanel {
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ControlPanel extends JFrame {
+    CanvasJPanel cjp;
+    
+    public ControlPanel()   {
+        super("Round 1");
+        
+        cjp = new CanvasJPanel();
+        getContentPane().setLayout(new BorderLayout());
+	getContentPane().add(cjp,"Center");
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setSize (640, 480);
+        setVisible(true);
+        
+    }
     
 }
--- Round1/src/app.java
+++ Round1/src/app.java
@@ -8,7 +8,7 @@ public class app {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Round1JFrame r1jf = new Round1JFrame();
+       ControlPanel r1jf = new ControlPanel();
     }
     
 }
